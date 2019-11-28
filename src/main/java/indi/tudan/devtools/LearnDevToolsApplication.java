973624064dda6f264d27977c9beaebc20fe19cb1
tudan110 <wtudan@126.com>
2019-11-28 17:09:46 +0800

    init

---
+++ src/main/java/indi/tudan/devtools/LearnDevToolsApplication.java
@@ -0,0 +1,13 @@
+package indi.tudan.devtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDevToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnDevToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/indi/tudan/devtools/LearnDevToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package indi.tudan.devtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDevToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
